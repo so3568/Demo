4e284c7fc3f3db399187219b1289ea3a1274e66f
ysoun <ysoun@DESKTOP-6R3K6S3>
2017-03-19 17:17:19 -0400

    US1 Add functionality implementation

--- src/main/java/com/company/demo/AddFunction.java
+++ src/main/java/com/company/demo/AddFunction.java
@@ -4,6 +4,11 @@ public class AddFunction {
 
 	public static void main(String[] args) {
 		System.out.println("Test demo");
+		int a, b;
+		a = 10;
+		b = 15;
+		int c = a + b;
+		System.out.println("sum=" + c);
 	}
 
 }
