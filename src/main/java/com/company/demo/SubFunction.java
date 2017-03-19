7711dacda010ceaba3d0c1d452c48c1de19729c5
so3568 <so.java3568@gmail.com>
2017-03-19 17:29:02 -0400

    US2 Sub Function Implementation

---
+++ src/main/java/com/company/demo/SubFunction.java
@@ -0,0 +1,13 @@
+package com.company.demo;
+
+public class SubFunction {
+
+	public static void main(String[] args) {
+		int a, b;
+		a = 10;
+		b = 15;
+		int c = a - b;
+		System.out.println("sub=" + c);
+	}
+
+}
