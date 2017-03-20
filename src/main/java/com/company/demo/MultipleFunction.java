2e4e49a4dd9ca9ff72e7dfdefecbbd0e2e179f46
ysoun <ysoun@DESKTOP-6R3K6S3>
2017-03-19 20:42:41 -0400

    US3 added multiple functionality

---
+++ src/main/java/com/company/demo/MultipleFunction.java
@@ -0,0 +1,14 @@
+package com.company.demo;
+
+public class MultipleFunction {
+
+	public static void main(String[] args) {
+		int a, b;
+		a = 10;
+		b = 15;
+		int c = a * b;
+		System.out.println("sub=" + c);
+
+	}
+
+}
